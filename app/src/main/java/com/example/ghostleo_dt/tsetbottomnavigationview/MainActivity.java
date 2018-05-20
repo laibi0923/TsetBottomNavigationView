f87300b1393b4316b22165f911f2a21c47c5951c
laibi0923 <laibi0923!@gmail.com>
2018-05-20 21:05:03 +0800

    update

---
+++ app/src/main/java/com/example/ghostleo_dt/tsetbottomnavigationview/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.ghostleo_dt.tsetbottomnavigationview;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FrameLayout mContent;
+    private TextView mContentMsg;
+    private BottomNavigationView mBottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mContent = (FrameLayout) findViewById(R.id.content);
+        mContentMsg = (TextView) findViewById(R.id.content_msg);
+        mBottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+
+                    case R.id.item1:
+                        mContentMsg.setText(item.getTitle());
+                        break;
+
+                    case R.id.item2:
+                        mContentMsg.setText(item.getTitle());
+                        break;
+
+                    case R.id.item3:
+                        mContentMsg.setText(item.getTitle());
+                        break;
+                }
+
+                return true;
+            }
+        });
+    }
+
+
+}
